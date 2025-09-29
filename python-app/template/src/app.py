26c2ab655ba47530eed749a756c6bb1388687c98
andrew sciametta <eggystrone88@gmail.com>
2025-09-29 12:15:52 -0700

    adds python app software template

---
+++ python-app/template/src/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({
+        'message': 'Python App is running!',
+        'endpoints': ['/api/v1/details', '/api/v1/healthz']
+    })
+
+@app.route('/api/v1/info')
+def info():
+    return jsonify({
+        'time': datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
+        'hostname': socket.gethostname(),
+        'message': 'You are doing great, humansz@!#3333#@@!',
+        'deployed_on': 'kubernetes',
+        'env': '${{values.app_env}}',
+        'app_name': '${{values.app_name}}'
+        })
+
+@app.route('/api/v1/healthz')
+def health():
+    return jsonify({'status': 'up'}), 200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
+
+
+
+
+#'/api/v1/details'
+#'/api/v1/healthz'
